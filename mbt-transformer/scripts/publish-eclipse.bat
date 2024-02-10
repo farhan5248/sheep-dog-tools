8cae612a21dbd5497b89b3e3a9eea7ec95c8f403
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-02-10 03:52:14 -0500

    Adding mvn install for cucumberextexteditor

---
+++ mbt-transformer/scripts/publish-eclipse.bat
@@ -0,0 +1,6 @@
+cd ..
+call mvn clean
+mvn dependency:copy-dependencies -DoutputDirectory=target/dependency
+mvn deploy:deploy-file -DgroupId=org.eclipse.core -DartifactId=runtime -Dversion=3.29.0.v20230726-0617 -Dpackaging=jar -Dfile=target/dependency -Durl=https://maven.pkg.github.com/farhan5248/claims-adjudication-ai -DrepositoryId=github
+cd scripts 
+
