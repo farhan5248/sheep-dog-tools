051674ce13d6337029ad6d6ea989f96913ab61be
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-02-21 16:58:04 -0500

    Allow keywords as ordinary tokens in descriptions or tables

--- cucumberxtexteditor.parent/cucumberxtexteditor/src/org/farhan/parser/antlr/internal/MyCucumberLexer.java
+++ cucumberxtexteditor.parent/cucumberxtexteditor/src/org/farhan/parser/antlr/internal/MyCucumberLexer.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 public class MyCucumberLexer extends InternalCucumberLexer {
 
 	private boolean isDocString = false;
+	private boolean lastTokenHasNewLine = true;
+	private boolean isTagLine = false;
 
 	public MyCucumberLexer() {
 	}
@@ -32,16 +34,59 @@ public class MyCucumberLexer extends InternalCucumberLexer {
 		if (tokenText == null) {
 			return t;
 		}
-		//System.out.println("Handling Token >>>" + tokenText + "<<< " + t.getType());
+		System.out.println("Handling Token >>>" + tokenText + "<<< " + t.getType());
+
+		// This handles keywords in a docstring
 		if (!isDocString && tokenText.contentEquals("\"\"\"")) {
 			isDocString = true;
 		} else if (isDocString && tokenText.contentEquals("\"\"\"")) {
 			isDocString = false;
 		} else if (isDocString && !tokenText.contentEquals("\"\"\"")) {
-			// TODO if the type is any of the keywords, change it to STRING
 			if (isKeyword(t.getType())) {
-				//System.out.println("Changing Type >>>" + tokenText + "<<<");
 				t.setType(RULE_STRING);
+			}
+		}
+		// This handles keywords not in a docstring
+		// If it's a identified as a keyword but doesn't have a new line on its left,
+		// then it should be a string
+		if (isKeyword(t.getType())) {
+			if (t.getType() == 15) {
+				// the | can occur multiple times on one line. It's not always
+				// preceded by new lines. This is escaped with \
+			} else {
+				if (lastTokenHasNewLine) {
+					// this is an actual keyword
+					if (t.getType() == 23) {
+						// if it's an @
+						isTagLine = true;
+					}
+				} else {
+					if (t.getType() == 23 && isTagLine) {
+						// if it's an @ and we're on a tag line, then treat it as a tag
+					} else {
+						t.setType(RULE_STRING);
+					}
+				}
+			}
+			lastTokenHasNewLine = false;
+		} else if (t.getType() == 0) {
+			// if it begins with "Scenario " but isn't Scenario Outline: then change
+			// it's type to RULE_STRING
+			if (tokenText.startsWith("Scenario ") && !tokenText.contentEquals("Scenario Outline:")) {
+				t.setType(RULE_STRING);
+			}
+			lastTokenHasNewLine = false;
+		} else {
+			if (tokenText.contains("\n")) {
+				lastTokenHasNewLine = true;
+				// we're leaving the tag line
+				isTagLine = false;
+			} else {
+				if (tokenText.replace(" ", "").length() == 0) {
+					// if it's all whitespace (and not Scenario O) ignore the whitespace
+				} else {
+					lastTokenHasNewLine = false;
+				}
 			}
 		}
 		return t;
