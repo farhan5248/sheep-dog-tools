c81f08d70899ccdab00aee9e4f48bc3ba6bc5718
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-02-23 14:27:42 -0500

    Upgrading dependency to snapshot versions

--- mbt-maven-plugin/scripts/release.bat
+++ mbt-maven-plugin/scripts/release.bat
@@ -2,9 +2,8 @@ cd ..
 call mvn clean
 call mvn release:clean
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false
-REM call mvn -B release:prepare -Dmaven.test.skip=true -DcheckModificationExcludeList=pom.xml,scripts/release.bat
-REM call mvn install -Dmaven.test.skip=true
-REM call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
-REM call git add pom.xml
-REM call git commit -m "Upgrading dependency versions"
+call mvn -B release:prepare -Dmaven.test.skip=true -DcheckModificationExcludeList=pom.xml,scripts/release.bat
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
+call git add pom.xml
+call git commit -m "Upgrading dependency versions"
 cd scripts 
