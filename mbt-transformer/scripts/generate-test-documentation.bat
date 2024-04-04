ac1c76c0c19ef68526de88c85ef8eeb39e574041
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-04-04 15:05:47 -0400

    Updating scripts with latest plug-in version

--- mbt-transformer/scripts/generate-test-documentation.bat
+++ mbt-transformer/scripts/generate-test-documentation.bat
@@ -1,6 +1,6 @@
 cd ..
 call mvn clean
-call mvn org.farhan:mbt-maven-plugin:1.7-SNAPSHOT:cucumber-to-uml -Dtag="%1"
-call mvn org.farhan:mbt-maven-plugin:1.7-SNAPSHOT:uml-to-graph -Dtag="%1"
-call mvn org.farhan:mbt-maven-plugin:1.7-SNAPSHOT:graph-to-asciidoctor -Dtag="%1"
+call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:cucumber-to-uml -Dtag="%1"
+call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:uml-to-graph -Dtag="%1"
+call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:graph-to-asciidoctor -Dtag="%1"
 cd scripts 
