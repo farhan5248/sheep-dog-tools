6414b166b5c733b51603b71dbed263b495eb1e12
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-08-30 16:23:25 -0400

    Updated script versions to LATEST

--- drug-claim-svc/scripts/generate-test-automation.bat
+++ drug-claim-svc/scripts/generate-test-automation.bat
@@ -1,5 +1,5 @@
 cd ..
 call mvn clean
-call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:asciidoctor-to-uml -Dtag="%1"
-call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:uml-to-cucumber -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:LATEST:asciidoctor-to-uml -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:LATEST:uml-to-cucumber -Dtag="%1"
 cd scripts 
--- sheep-dog-test/scripts/generate-test-documentation.bat
+++ sheep-dog-test/scripts/generate-test-documentation.bat
@@ -1,5 +1,5 @@
 cd ..
 call mvn clean
-call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:cucumber-to-uml -Dtag="%1"
-call mvn org.farhan:mbt-maven-plugin:1.8-SNAPSHOT:uml-to-asciidoctor -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:LATEST:cucumber-to-uml -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:LATEST:uml-to-asciidoctor -Dtag="%1"
 cd scripts 
