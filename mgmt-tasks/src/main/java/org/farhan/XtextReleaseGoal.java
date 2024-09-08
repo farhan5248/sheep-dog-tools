106856f28727d62fe8dbf8aa924d0b57a04dd921
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-09-07 23:47:30 -0400

    Updating currentVersion and nextVersion

--- mgmt-tasks/src/main/java/org/farhan/XtextReleaseGoal.java
+++ mgmt-tasks/src/main/java/org/farhan/XtextReleaseGoal.java
@@ -16,10 +16,10 @@ public class XtextReleaseGoal {
 
 	public static void main(String[] args) {
 
-		String currentVersion = "1.17.0";
-		String nextVersion = "1.18.0";
-		String currentSheepDogTestVersion = "1.1";
-		String nextSheepDogTestVersion = "1.2";
+		String currentVersion = "1.18.0";
+		String nextVersion = "1.19.0";
+		String currentSheepDogTestVersion = "1.2";
+		String nextSheepDogTestVersion = "1.3";
 
         // updateToRelease(currentVersion, currentSheepDogTestVersion);
 		// Run mvn install
