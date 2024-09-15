14822fd1c802faa85ec67d77e103ea7255501be7
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-09-14 21:25:59 -0400

    Code generation

--- sheepdogxtextcukeplugin.parent/sheepdogxtextcukeplugin/src/org/farhan/mbt/CucumberRuntimeModule.java
+++ sheepdogxtextcukeplugin.parent/sheepdogxtextcukeplugin/src/org/farhan/mbt/CucumberRuntimeModule.java
@@ -3,9 +3,22 @@
  */
 package org.farhan.mbt;
 
+import org.eclipse.xtext.generator.IOutputConfigurationProvider;
+import org.farhan.mbt.generator.CucumberOutputConfigurationProvider;
+
+import com.google.inject.Binder;
+import com.google.inject.Singleton;
 
 /**
- * Use this class to register components to be used at runtime / without the Equinox extension registry.
+ * Use this class to register components to be used at runtime / without the
+ * Equinox extension registry.
  */
 public class CucumberRuntimeModule extends AbstractCucumberRuntimeModule {
+
+	@Override
+	public void configure(Binder binder) {
+		super.configure(binder);
+		binder.bind(IOutputConfigurationProvider.class).to(CucumberOutputConfigurationProvider.class)
+				.in(Singleton.class);
+	}
 }
