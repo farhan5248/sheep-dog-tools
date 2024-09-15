00a007885a4bd9098af3e765cc961647ebf86580
Farhan Sheikh <farhan.sheikh@mcmaster.ca>
2024-09-15 02:54:18 -0400

    Upgrading script versions

--- sheep-dog-dev/scripts/generate-test-documentation.bat
+++ sheep-dog-dev/scripts/generate-test-documentation.bat
@@ -1,5 +1,5 @@
 cd ..
 call mvn clean
-call mvn org.farhan:sheep-dog-maven-plugin:1.15-SNAPSHOT:cucumber-to-uml -Dtag="%1"
-call mvn org.farhan:sheep-dog-maven-plugin:1.15-SNAPSHOT:uml-to-asciidoctor -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:1.16-SNAPSHOT:cucumber-to-uml -Dtag="%1"
+call mvn org.farhan:sheep-dog-maven-plugin:1.16-SNAPSHOT:uml-to-asciidoctor -Dtag="%1"
 cd scripts 
