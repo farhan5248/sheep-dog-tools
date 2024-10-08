e90997e8db44ed58f78226ccf7bc365b199969fa
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2024-10-08 16:08:55 -0400

    Refreshed step library code

--- sheep-dog-test/src/test/java/org/farhan/stepdefs/xtext/XtextScenarioValidationActionSteps.java
+++ sheep-dog-test/src/test/java/org/farhan/stepdefs/xtext/XtextScenarioValidationActionSteps.java
@@ -6,13 +6,6 @@ import io.cucumber.datatable.DataTable;
 
 public class XtextScenarioValidationActionSteps {
 
-    @Given("^The scenario validation action is performed$")
-    public void theScenarioValidationActionIsPerformed() {
-        XtextFactory.get("ScenarioValidationAction").setComponent("xtext");
-        XtextFactory.get("ScenarioValidationAction").setPath("scenario validation");
-        XtextFactory.get("ScenarioValidationAction").transition();
-    }
-
     @Given("^The scenario validation action is performed as follows$")
     public void theScenarioValidationActionIsPerformedAsFollows(DataTable dataTable) {
         XtextFactory.get("ScenarioValidationAction").setComponent("xtext");
