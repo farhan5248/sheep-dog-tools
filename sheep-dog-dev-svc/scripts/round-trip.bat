36e4b9529d9b389dd6f1a2f21583be8b0b9c122e
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2024-10-23 15:32:28 -0400

    Convert all feature file tests to adoc files

---
+++ sheep-dog-dev-svc/scripts/round-trip.bat
@@ -0,0 +1,7 @@
+cd ..
+call mvn clean
+call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.1-SNAPSHOT:cucumber-to-uml -Dtag="%1"
+call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.1-SNAPSHOT:uml-to-asciidoctor -Dtag="%1"
+call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.1-SNAPSHOT:asciidoctor-to-uml -Dtag="%1"
+call mvn org.farhan:sheep-dog-dev-svc-maven-plugin:1.1-SNAPSHOT:uml-to-cucumber -Dtag="%1"
+cd scripts 
