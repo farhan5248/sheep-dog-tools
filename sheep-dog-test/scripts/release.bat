fd21e8722500a4148390bfd966eb0b57a747b937
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2024-10-22 21:44:31 -0400

    Updated release.bat

--- sheep-dog-test/scripts/release.bat
+++ sheep-dog-test/scripts/release.bat
@@ -2,7 +2,7 @@ cd ..
 call mvn clean
 call mvn release:clean
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false
-call mvn -B release:prepare -Dmaven.test.skip=true -DcheckModificationExcludeList=pom.xml
+call mvn -B -Darguments=-DskipTests release:prepare -DcheckModificationExcludeList=pom.xml
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
 call git add pom.xml
 call git commit -m "Upgrading dependency versions"
