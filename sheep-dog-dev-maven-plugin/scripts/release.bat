ded24c3aeaa02af3b6b850c372d6760da95c0d59
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2024-11-14 14:50:10 -0500

    Testing maven release:perform

--- sheep-dog-dev-maven-plugin/scripts/release.bat
+++ sheep-dog-dev-maven-plugin/scripts/release.bat
@@ -1,7 +1,7 @@
 cd ..
 call mvn clean
 call mvn release:clean
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
 call mvn -B -Darguments=-DskipTests release:prepare -DcheckModificationExcludeList=pom.xml
 call mvn -B -Darguments=-DskipTests release:perform -DlocalCheckout=true -Dgoals=install
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
