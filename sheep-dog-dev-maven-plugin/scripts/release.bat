849273a7b84e3f4b8980af93aa2ca755b2872b0a
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2024-11-14 15:08:08 -0500

    Testing maven release:perform

--- sheep-dog-dev-maven-plugin/scripts/release.bat
+++ sheep-dog-dev-maven-plugin/scripts/release.bat
@@ -7,6 +7,6 @@ call mvn -B -Darguments=-DskipTests release:perform -DlocalCheckout=true -Dgoals
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
 call git add pom.xml
 call git commit -m "Upgrading dependency versions"
-call mvn install -DskipTests
+call mvn clean install -DskipTests
 call mvn clean
 cd scripts 
--- sheep-dog-test/scripts/release.bat
+++ sheep-dog-test/scripts/release.bat
@@ -1,10 +1,12 @@
 cd ..
 call mvn clean
 call mvn release:clean
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
 call mvn -B -Darguments=-DskipTests release:prepare -DcheckModificationExcludeList=pom.xml
+call mvn -B -Darguments=-DskipTests release:perform -DlocalCheckout=true -Dgoals=install
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
 call git add pom.xml
 call git commit -m "Upgrading dependency versions"
-call mvn install -DskipTests
+call mvn clean install -DskipTests
+call mvn clean
 cd scripts 
