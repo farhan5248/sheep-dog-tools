aef3bc95405185263ade363484b527050eda320d
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-01-04 02:13:13 -0500

    Create GitHub Action to Release all artifacts

--- sheep-dog-test/scripts/release.bat
+++ sheep-dog-test/scripts/release.bat
@@ -1,9 +1,7 @@
 cd ..
-call mvn clean
-call mvn release:clean
+call mvn clean release:clean
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn -B -Darguments=-DskipTests release:prepare -DcheckModificationExcludeList=pom.xml
-call mvn -B -Darguments=-DskipTests release:perform -DlocalCheckout=true -Dgoals=install
+call mvn -B release:prepare -DcheckModificationExcludeList=pom.xml -DpreparationGoals=deploy
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
 call git add pom.xml
 call git commit -m "Upgrading dependency versions"
