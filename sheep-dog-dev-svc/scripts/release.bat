ed81ca147e438bb260469d95092339f9d5686b66
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-01-04 21:39:34 -0500

    Create GitHub Action to Release all artifacts

--- sheep-dog-dev-svc/scripts/release.bat
+++ sheep-dog-dev-svc/scripts/release.bat
@@ -1,9 +1,10 @@
 cd ..
-call mvn clean release:clean
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
+call mvn release:clean
 call mvn -B release:prepare -DcheckModificationExcludeList=pom.xml -DpreparationGoals=deploy
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
+call mvn deploy
 call git add pom.xml
 call git commit -m "Upgrading dependency versions"
-call mvn deploy
+call git push
 cd scripts 
--- sheepdogxtextcukeplugin.parent/scripts/release.bat
+++ sheepdogxtextcukeplugin.parent/scripts/release.bat
@@ -1,8 +1,8 @@
 cd ..
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.5:prepare -DpreparationGoals=clean,deploy
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.13:prepare -DpreparationGoals=deploy
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
-call git add */pom.xml
-call git commit -m "Upgrading dependency versions"
 call mvn deploy
+call git add *.xml
+call git commit -m "Upgrading dependency versions"
 cd scripts 
