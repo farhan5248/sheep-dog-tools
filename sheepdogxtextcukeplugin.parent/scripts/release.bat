bf32d03397ab7737d7dffcabc45c335e24bd7b2b
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-01-04 22:07:06 -0500

    Create GitHub Action to Release all artifacts

--- sheepdogxtextcukeplugin.parent/scripts/release.bat
+++ sheepdogxtextcukeplugin.parent/scripts/release.bat
@@ -1,6 +1,6 @@
 cd ..
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.13:prepare -DpreparationGoals=deploy
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.14:prepare -DpreparationGoals=deploy
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
 call mvn deploy
 call git add *.xml
