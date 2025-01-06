e92bdb0c66fb328a31d78ecf667e52d090072ef4
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-01-05 23:34:41 -0500

    Make release.yml file faster

--- sheepdogxtextcukeplugin.parent/scripts/release.bat
+++ sheepdogxtextcukeplugin.parent/scripts/release.bat
@@ -1,8 +1,10 @@
 cd ..
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
 call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.14:prepare -DpreparationGoals=deploy
+call git push
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
-call mvn deploy
 call git add *
 call git commit -m "Upgrading dependency versions"
+call git push
+call mvn deploy -DskipTests
 cd scripts 
