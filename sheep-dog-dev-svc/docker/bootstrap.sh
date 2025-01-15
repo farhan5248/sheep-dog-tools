47a84e145b645756ad75e7c9f8bce1b7a768a536
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-01-15 15:50:51 -0500

    Update Maven and GitHub Automation with Minikube

--- sheep-dog-dev-svc/docker/bootstrap.sh
+++ sheep-dog-dev-svc/docker/bootstrap.sh
@@ -12,11 +12,11 @@ cnt=$(echo "select user from mysql.user where user = 'mbt';" | mysql -u root -pr
 if [ $cnt -eq 0 ]; then
 # TODO move this all to a single sql script
   echo "Creating MBT user"
-  echo "create user 'mbt'@'%' identified by 'mbt';" | mysql -u root -proot
+  echo "CREATE USER 'mbt'@'%' IDENTIFIED BY 'mbt';" | mysql -u root -proot
   echo "Creating MBT database"
-  echo "create database mbt;" | mysql -u root -proot
+  echo "CREATE DATABASE mbt;" | mysql -u root -proot
   echo "Granting all privileges on MBT database to MBT user"
-  echo "GRANT ALL PRIVILEGES ON mbt.* TO 'mbt';" | mysql -u root -proot
+  echo "GRANT ALL PRIVILEGES ON mbt.* TO 'mbt'@'%';" | mysql -u root -proot
 fi
 
 exit 0
