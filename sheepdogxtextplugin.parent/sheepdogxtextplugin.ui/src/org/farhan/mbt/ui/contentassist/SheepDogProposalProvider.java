2a259c15417cfdc2330010cb81682868898cddd4
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-02-11 22:23:48 -0500

    Add Xtext Asciidoc content assist

--- sheepdogxtextplugin.parent/sheepdogxtextplugin.ui/src/org/farhan/mbt/ui/contentassist/SheepDogProposalProvider.java
+++ sheepdogxtextplugin.parent/sheepdogxtextplugin.ui/src/org/farhan/mbt/ui/contentassist/SheepDogProposalProvider.java
@@ -3,10 +3,130 @@
  */
 package org.farhan.mbt.ui.contentassist;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map.Entry;
+
+import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.ui.editor.contentassist.ConfigurableCompletionProposal;
+import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
+import org.farhan.helper.Proposal;
+import org.farhan.helper.StepDefinitionHelper;
+import org.farhan.mbt.LanguageAccessImpl;
+import org.farhan.mbt.sheepDog.And;
+import org.farhan.mbt.sheepDog.Given;
+import org.farhan.mbt.sheepDog.Row;
+import org.farhan.mbt.sheepDog.Step;
+import org.farhan.mbt.sheepDog.Table;
+import org.farhan.mbt.sheepDog.Then;
+import org.farhan.mbt.sheepDog.When;
 
 /**
- * See https://www.eclipse.org/Xtext/documentation/310_eclipse_support.html#content-assist
+ * See
+ * https://www.eclipse.org/Xtext/documentation/310_eclipse_support.html#content-assist
  * on how to customize the content assistant.
  */
 public class SheepDogProposalProvider extends AbstractSheepDogProposalProvider {
+
+	public void completeGiven_TheStepTable(Given step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeGiven_TheStepTable(step, assignment, context, acceptor);
+		completeTheStepTable(step, assignment, context, acceptor);
+	}
+
+	public void completeWhen_TheStepTable(When step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeWhen_TheStepTable(step, assignment, context, acceptor);
+		completeTheStepTable(step, assignment, context, acceptor);
+	}
+
+	public void completeThen_TheStepTable(Then step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeThen_TheStepTable(step, assignment, context, acceptor);
+		completeTheStepTable(step, assignment, context, acceptor);
+	}
+
+	public void completeAnd_TheStepTable(And step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeAnd_TheStepTable(step, assignment, context, acceptor);
+		completeTheStepTable(step, assignment, context, acceptor);
+	}
+
+	public void completeStepTable_Rows(Table stepTable, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		// TODO automatically create columns to match header
+	}
+
+	public void completeRow_Cells(Row row, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		// TODO if it's the first row, suggest a name based on all names used in the
+		// step definition parameters for this step definition
+	}
+
+	public void completeGiven_Name(Given step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeGiven_Name(step, assignment, context, acceptor);
+		completeName(step, assignment, context, acceptor);
+	}
+
+	public void completeWhen_Name(When step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeWhen_Name(step, assignment, context, acceptor);
+		completeName(step, assignment, context, acceptor);
+	}
+
+	public void completeThen_Name(Then step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeThen_Name(step, assignment, context, acceptor);
+		completeName(step, assignment, context, acceptor);
+	}
+
+	public void completeAnd_Name(And step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completeAnd_Name(step, assignment, context, acceptor);
+		completeName(step, assignment, context, acceptor);
+	}
+
+	private void completeTheStepTable(Step step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		try {
+			for (Entry<String, Proposal> p : StepDefinitionHelper.proposeStepTable(new LanguageAccessImpl(step))
+					.entrySet()) {
+				ConfigurableCompletionProposal proposal = (ConfigurableCompletionProposal) createCompletionProposal(
+						p.getValue().getReplacement(), p.getValue().getDisplay(), null, context);
+				if (proposal != null) {
+					proposal.setAdditionalProposalInfo(p.getValue().getDocumentation());
+					acceptor.accept(proposal);
+				}
+			}
+		} catch (Exception e) {
+			logError(e, step.getName());
+		}
+	}
+
+	private void completeName(Step step, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		try {
+			// TODO rename propose to proposeStep
+			for (Entry<String, Proposal> p : StepDefinitionHelper.propose(new LanguageAccessImpl(step)).entrySet()) {
+				ConfigurableCompletionProposal proposal = (ConfigurableCompletionProposal) createCompletionProposal(
+						p.getValue().getReplacement(), p.getValue().getDisplay(), null, context);
+				if (proposal != null) {
+					proposal.setAdditionalProposalInfo(p.getValue().getDocumentation());
+					acceptor.accept(proposal);
+				}
+			}
+		} catch (Exception e) {
+			logError(e, step.getName());
+		}
+	}
+
+	private void logError(Exception e, String name) {
+		// TODO inject the logger instead
+		System.out.println("There was an error listing proposals for: " + name);
+		StringWriter sw = new StringWriter();
+		e.printStackTrace(new PrintWriter(sw));
+		System.out.println(sw.toString());
+	}
 }
