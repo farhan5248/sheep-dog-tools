ed80d793625e7ac25b7660e8a802245711005434
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2025-04-03 17:33:27 -0400

    Disable running tests in release workflows

--- sheepdogcucumber.parent/scripts/release.bat
+++ sheepdogcucumber.parent/scripts/release.bat
@@ -3,7 +3,7 @@ call git reset --hard HEAD
 call git clean -fdx
 call git pull
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn -B release:prepare -DcheckModificationExcludeList=pom.xml -DpreparationGoals=deploy -DskipTests
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:1.14:prepare -DpreparationGoals=deploy,-DskipTests
 call git push
 call git push --tags
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
